5df35cd4db5627579353d4cc2c4687dfd109e8ca
Slobodan Klentikov <slobodan.kletnikov@uniqcast.com>
2024-01-31 20:32:25 +0400

    Update stream

--- receiver/src/index.ts
+++ receiver/src/index.ts
@@ -16,11 +16,11 @@ setTimeout(() => {
     setTimeout(() => {
       playerLogger.debug("set src");
       const url =
-        "https://edge2.salomtv.uz:11610/auth-streaming/3,b7fa9376a92877a97c7e56eb63849d02e172113a,1706800362,nauman,0-A4654-hls-UNIQCAST,8,8,8,8,8,8,8,8,DESKTOP,318,all,none,default,2.51.51.90/hls/vod/0-A4654-hls-UNIQCAST/playlist.m3u8";
+        "https://edge1.salomtv.uz:11610/auth-streaming/3,1e40dbf0de37cb046fb5eb566a4728b8ddd1902c,1706805078,nauman,0-A4654-hls-UNIQCAST,8,8,8,8,8,8,8,8,DESKTOP,318,all,none,default,2.51.51.90/hls/vod/0-A4654-hls-UNIQCAST/playlist.m3u8";
       const licence =
-        "https://edge2.salomtv.uz:11610/drmproxy/wv/license?lat=n2wSNPubbFa3KJEJKdI2QuqEKSBe8h8AXiLPm%2B%2BhEGoPTVnF9GmIN%2FZAUDONkJ3nmVFRb8YEWtV92N4qrxaaQD%2FIwuCFWPGb2IYzXaAOQNmXgjL9d%2BHNq0eT6BuFssXlYbebHGJ40QrLir%2Fs5tWs%2FHBoGav1hKCzyJPWlhCI1RURXCZzoOZEuqA7zUB1tvbY5cG5sITCb4b0Mx2JZbCHsOqAEfWJmDX3EcquXx1MLrPl9gFfHeCIdT7h8caAegQ7";
+        "https://edge1.salomtv.uz:11610/drmproxy/wv/license?lat=n2wSNPubbFa3KJEJKdI2QuqEKSBe8h8AXiLPm%2B%2BhEGoPTVnF9GmIN%2FZAUDONkJ3nmVFRb8YEWtV92N4qrxaaQD%2FIwuCFWPGb2IYzXaAOQNmXgjL9d%2BHNq0eT6BuFssXlMerxOJQqJGIa6FLNAKvYP4oFgk2NUluEdKWbc%2Flmh7cTitGBzpCENjqCbW2Y1R6WbFXpw%2Bmfydxydz%2F9oygrQT03q50K3QcYVrITQULRJfyT9C9f%2F3eEi5%2BYBFCv72vy";
       const jwt =
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY3MTU2NjksImlhdCI6MTcwNjcxMzg2OSwiaXNzIjoidW5pcUNhc3QiLCJzdWIiOiJhY2Nlc3MiLCJkaWQiOjMxOCwiZHVpZCI6IjJhOTJkM2UwLWI1MmYtMTFlZS05NGFjLTQzYTlmYzVjOWE0NCIsIm9pZCI6MSwib3VpZCI6ImRlZmF1bHQiLCJyaWQiOjEsInJvbGUiOlsic3Vic2NyaWJlciJdLCJydWlkIjoiZGVmYXVsdCIsInVpZCI6MSwidmVyc2lvbiI6Mn0.Rt7CRO-LW-PhpNrymzqdQ-Hyk81P-xuk4yU1Ikz9vyo";
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY3MjA1MDYsImlhdCI6MTcwNjcxODcwNiwiaXNzIjoidW5pcUNhc3QiLCJzdWIiOiJhY2Nlc3MiLCJkaWQiOjMxOCwiZHVpZCI6IjJhOTJkM2UwLWI1MmYtMTFlZS05NGFjLTQzYTlmYzVjOWE0NCIsIm9pZCI6MSwib3VpZCI6ImRlZmF1bHQiLCJyaWQiOjEsInJvbGUiOlsic3Vic2NyaWJlciJdLCJydWlkIjoiZGVmYXVsdCIsInVpZCI6MSwidmVyc2lvbiI6Mn0.lR2xLytB9CxOGPRh2YuEx6YCx15sxHOWQuxSJTrHeAM";
       player
         .setSrc(url, {
           type: "widevine",
