46dc5f56e7d3f084d87d9130daed5ed51bc8f3d8
Slobodan Klentikov <slobodan.kletnikov@uniqcast.com>
2024-02-02 11:11:06 +0400

    Update build

--- receiver/build/index.js
+++ receiver/build/index.js
@@ -9,9 +9,9 @@ setTimeout(function () {
         playerLogger.debug("player initialized");
         setTimeout(function () {
             playerLogger.debug("set src");
-            var url = "https://edge3.salomtv.uz:11610/auth-streaming/3,f048c2f0141b30e3fab1a382d2693db34a50ae1a,1706941571,nauman,0-A4684-hls-UNIQCAST,8,8,8,8,8,8,8,8,DESKTOP,318,all,none,default,87.201.183.110/hls/vod/0-A4684-hls-UNIQCAST/playlist.m3u8";
-            var license = "https://edge3.salomtv.uz:11610/drmproxy/wv/license?lat=IkAuKAFqUQmA8j0mvclAkIDxWY6zX0id6Clbv20%2BCm8WoGUNdR9jhdUXYBJjZG0udKDceFXUVRB3WUtICkJPHMJZWggBptDLRYki%2BqBSgox82Sbq7LuQ8UaS91EmuKwGecpauLof2tpfGy1RnSj2Y9yJw3V05ahPjK3IqN0Gv8K2SOcotRDrTmz6YgLBn0HRH3HWzkDOG5rQmx2U6%2FJe1zkuIpjFINMSeSsYAP1M8XAEsX468oMUD1nRqnM%2BEo1l";
-            var jwt = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY4NTY1NjAsImlhdCI6MTcwNjg1NDc2MCwiaXNzIjoidW5pcUNhc3QiLCJzdWIiOiJhY2Nlc3MiLCJkaWQiOjMxOCwiZHVpZCI6IjJhOTJkM2UwLWI1MmYtMTFlZS05NGFjLTQzYTlmYzVjOWE0NCIsIm9pZCI6MSwib3VpZCI6ImRlZmF1bHQiLCJyaWQiOjEsInJvbGUiOlsic3Vic2NyaWJlciJdLCJydWlkIjoiZGVmYXVsdCIsInVpZCI6MSwidmVyc2lvbiI6Mn0.fjOc2IeMxSyF3My_ZZVQE8u5AJMrXSM1W8nF3-HxrMM";
+            var url = "https://edge2.salomtv.uz:11610/auth-streaming/3,1caf8af248219cda3f68c84c7cc22d4c06088861,1706944190,nauman,0-A4662-hls-UNIQCAST,8,8,8,8,8,8,8,8,DESKTOP,318,all,none,default,87.201.183.110/hls/vod/0-A4662-hls-UNIQCAST/playlist.m3u8";
+            var license = "https://edge2.salomtv.uz:11610/drmproxy/wv/license?lat=IkAuKAFqUQmA8j0mvclAkFGBuC3oGgdwEKfdUCywMdg2ls1PK%2B%2FhEN%2FflG6sl64p8uOf5eOexzJAuZoQl1a3LYzOUc4yxSRS0GI4P5cOcnzlx4BSSNeH4%2BINun2VLzuR9MuS1UyhTGlx3XX1Dc0et7Vj89V5RFpD4wPX5DOzWv9unoAdHm811EwVVG0HvyZZpile0zOdXbkkoU0nSd4H32TgmA7Z%2FGXV8QS5PFQgedI%3D";
+            var jwt = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY4NTk1NjcsImlhdCI6MTcwNjg1Nzc2NywiaXNzIjoidW5pcUNhc3QiLCJzdWIiOiJhY2Nlc3MiLCJkaWQiOjMxOCwiZHVpZCI6IjJhOTJkM2UwLWI1MmYtMTFlZS05NGFjLTQzYTlmYzVjOWE0NCIsIm9pZCI6MSwib3VpZCI6ImRlZmF1bHQiLCJyaWQiOjEsInJvbGUiOlsic3Vic2NyaWJlciJdLCJydWlkIjoiZGVmYXVsdCIsInVpZCI6MSwidmVyc2lvbiI6Mn0.8dLhHFuzV5uraHfNbEU3Z5KY4kbjQ-0404PJkps_9T4";
             player
                 .setSrc(url, {
                 type: "widevine",
@@ -45,4 +45,6 @@ playerManager.addEventListener(cast.framework.events.EventType.ERROR, function (
         playerLogger.error("LOAD_FAILED: Verify the load request is set up " + "properly and the media is able to play.");
     }
 });
-context.start();
+var castReceiverOptions = new cast.framework.CastReceiverOptions();
+castReceiverOptions.skipPlayersLoad = true;
+context.start(castReceiverOptions);
