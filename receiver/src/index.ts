1bf60599d381033cf28ccc68971c9642c7f7c6b5
Slobodan Klentikov <slobodan.kletnikov@uniqcast.com>
2024-02-02 17:23:00 +0400

    asd

--- receiver/src/index.ts
+++ receiver/src/index.ts
@@ -18,7 +18,7 @@ setTimeout(() => {
       const url =
         "https://edge1.salomtv.uz:11610/auth-streaming/3,4f6401171fa250c921e7496b96477673c08388b5,1706966224,nauman,0-A4674-hls-UNIQCAST,8,8,8,8,8,8,8,8,DESKTOP,318,all,none,default,87.201.183.110/hls/vod/0-A4674-hls-UNIQCAST/playlist.m3u8";
       const license =
-        "https://edge2.salomtv.uz:11610/drmproxy/wv/license?lat=n2wSNPubbFa3KJEJKdI2QuqEKSBe8h8AXiLPm%2B%2BhEGoPTVnF9GmIN%2FZAUDONkJ3nmVFRb8YEWtV92N4qrxaaQD%2FIwuCFWPGb2IYzXaAOQNmXgjL9d%2BHNq0eT6BuFssXlE5af2CR0emte%2B6mKZ%2FWovHLm65hxdwP01miqL4eyg0k8G11L13%2B%2BEkwYE1Hoxpla91A4gYg6eNSU5pikhzCXKJqQmkvWQ%2BXI6Hq%2Bh%2B9R%2FUdpIcgLXmJwhDZpUiZNQnpt";
+        "https://edge1.salomtv.uz:11610/drmproxy/wv/license?lat=IkAuKAFqUQmA8j0mvclAkO5NLygYpPGYNH%2FHik%2FMDakObuEApWxbOjRM13HTpYtZHiC4Kp%2FScVH0Md%2FVbd4cbqW2imywRqD4RSOCIOq4TRY4ZIzM0Aiejq6LH%2BrEsDbESa4QtvzLtEH%2Bz5AAFmQGPtQTpY2ePugl5rb0z5Eno3JrchfN%2BdXMHeEwJ2rm1Naej3nLb8biV2U2HHEFS0V9lZA%2BOwxp1jZYX1ySaHUio6%2Fg8ILFG3wUh3Kma219Fjpk";
       const jwt =
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY4ODE2MTksImlhdCI6MTcwNjg3OTgxOSwiaXNzIjoidW5pcUNhc3QiLCJzdWIiOiJhY2Nlc3MiLCJkaWQiOjMxOCwiZHVpZCI6IjJhOTJkM2UwLWI1MmYtMTFlZS05NGFjLTQzYTlmYzVjOWE0NCIsIm9pZCI6MSwib3VpZCI6ImRlZmF1bHQiLCJyaWQiOjEsInJvbGUiOlsic3Vic2NyaWJlciJdLCJydWlkIjoiZGVmYXVsdCIsInVpZCI6MSwidmVyc2lvbiI6Mn0.BRd9yz7ODH7MPYA5FeffF3eB6cEDr9894Xsg-YBxXGc";
       player
