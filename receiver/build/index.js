619b5fea78fd67a8a7c2ea062b0a04407d42a1e7
Slobodan Klentikov <slobodan.kletnikov@uniqcast.com>
2024-02-14 14:41:19 +0400

    url

--- receiver/build/index.js
+++ receiver/build/index.js
@@ -9,19 +9,9 @@ setTimeout(function () {
         playerLogger.debug("player initialized");
         setTimeout(function () {
             playerLogger.debug("set src");
-            var url = "https://edge1.salomtv.uz:11610/auth-streaming/3,4f6401171fa250c921e7496b96477673c08388b5,1706966224,nauman,0-A4674-hls-UNIQCAST,8,8,8,8,8,8,8,8,DESKTOP,318,all,none,default,87.201.183.110/hls/vod/0-A4674-hls-UNIQCAST/playlist.m3u8";
-            var license = "https://edge1.salomtv.uz:11610/drmproxy/wv/license?lat=IkAuKAFqUQmA8j0mvclAkO5NLygYpPGYNH%2FHik%2FMDakObuEApWxbOjRM13HTpYtZHiC4Kp%2FScVH0Md%2FVbd4cbqW2imywRqD4RSOCIOq4TRY4ZIzM0Aiejq6LH%2BrEsDbESa4QtvzLtEH%2Bz5AAFmQGPtQTpY2ePugl5rb0z5Eno3JrchfN%2BdXMHeEwJ2rm1Naej3nLb8biV2U2HHEFS0V9lZA%2BOwxp1jZYX1ySaHUio6%2Fg8ILFG3wUh3Kma219Fjpk";
-            var jwt = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDY4ODIyNjksImlhdCI6MTcwNjg4MDQ2OSwiaXNzIjoidW5pcUNhc3QiLCJzdWIiOiJhY2Nlc3MiLCJkaWQiOjMxOCwiZHVpZCI6IjJhOTJkM2UwLWI1MmYtMTFlZS05NGFjLTQzYTlmYzVjOWE0NCIsIm9pZCI6MSwib3VpZCI6ImRlZmF1bHQiLCJyaWQiOjEsInJvbGUiOlsic3Vic2NyaWJlciJdLCJydWlkIjoiZGVmYXVsdCIsInVpZCI6MSwidmVyc2lvbiI6Mn0.VY8y4z-Hh4YhBktIKmba4D5jaARlzk4mpOoTxlGzEKM";
+            var url = "https://storage.googleapis.com/cpe-sample-media/content/big_buck_bunny/prog/big_buck_bunny_prog.mp4";
             player
-                .setSrc(url, {
-                type: "widevine",
-                data: {
-                    licenseUrl: license,
-                },
-                headers: {
-                    Authorization: jwt,
-                },
-            })
+                .setSrc(url)
                 .then(function () {
                 playerLogger.debug("play");
                 setTimeout(function () {
