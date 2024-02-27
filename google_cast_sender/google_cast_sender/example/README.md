84c6b1a33eff08998d4436e8c4c95b3be37dd069
Slobodan Klentikov <slobodan.kletnikov@uniqcast.com>
2024-02-27 10:23:46 +0400

    Remove google cast flutter plugin

--- google_cast_sender/google_cast_sender/example/README.md
+++
@@ -1,3 +0,0 @@
-# google_cast_sender_example
-
-Demonstrates how to use the google_cast_sender plugin.
--- google_cast_sender/google_cast_sender/example/android/app/src/main/kotlin/com/doozyx/plugins/google_cast_sender_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.doozyx.plugins.google_cast_sender_example
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity()
